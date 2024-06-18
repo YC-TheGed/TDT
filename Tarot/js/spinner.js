8ded911408f01add3926e5edbcdd9129cfbef68d
YC-TheGed <167610411+YC-TheGed@users.noreply.github.com>
2024-06-18 17:00:10 +0800

    .

--- Tarot/js/spinner.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("preloader").style.display = "flex";
-
-window.addEventListener("load", function () {
-  // Hide the preloader
-  document.getElementById("preloader").style.display = "none";
-
-  // Show the page content
-  document.querySelector("body").style.visibility = "visible";
-});
