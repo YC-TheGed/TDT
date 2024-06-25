85cd9f223b92cd78aac685c02a5ab6552eae5e80
YC-TheGed <167610411+YC-TheGed@users.noreply.github.com>
2024-06-25 16:06:35 +0800

    Update subscribe.php

--- Tarot/result/subscribe.php
+++ Tarot/result/subscribe.php
@@ -1,26 +1,21 @@
 <?php
 header('Content-Type: application/json');
 
-// Get email and first name from the POST data
 $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
 $firstName = filter_input(INPUT_POST, 'firstName', FILTER_SANITIZE_STRING);
 
-// HubSpot API Credentials
 $access_token = 'pat-na1-0f82d7e1-1c51-454a-939a-86e84dce50f7';
 $hubspot_api_url = 'https://api.hubapi.com/crm/v3/objects/contacts';
 
-// Prepare the contact properties
 $properties = [
     'email' => $email,
     'firstname' => $firstName
 ];
 
-// Prepare the request data
 $request_body = json_encode([
     'properties' => $properties
 ]);
 
-// Set up cURL request
 $ch = curl_init($hubspot_api_url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_HTTPHEADER, [
@@ -30,12 +25,10 @@ curl_setopt($ch, CURLOPT_HTTPHEADER, [
 curl_setopt($ch, CURLOPT_POST, true);
 curl_setopt($ch, CURLOPT_POSTFIELDS, $request_body);
 
-// Execute the request
 $response = curl_exec($ch);
 $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 curl_close($ch);
 
-// Check the response
 if ($http_code == 201) {
     echo json_encode(['status' => 'success', 'message' => 'Contact added to HubSpot!']);
 } else {
