6e7745344d86b7cd800c0289c0e3383d43ba32b7
YC-TheGed <167610411+YC-TheGed@users.noreply.github.com>
2024-06-27 13:28:54 +0800

    Update feeling.js

--- Tarot/result/js/feeling.js
+++ Tarot/result/js/feeling.js
@@ -20,7 +20,7 @@ window.addEventListener("load", function () {
     console.log("Sending data to PHP script:", email, firstName);
 
     // Send data using fetch
-    fetch("subscribe.php", {
+    fetch("/result/subscribe.php", {
       method: "POST",
       body: formData,
     })
